72efa70d041efd4fb1a37dd49de404e6f9a4def6
Alexander <35337991+baffoRti@users.noreply.github.com>
2023-10-27 13:39:59 +0300

    Update main.py
    Pylint ацтань x4

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""Pylint ацтань x1"""
 from fastapi import FastAPI, status
 import redis
 import uvicorn
@@ -10,16 +11,19 @@ conn = redis.from_url(f'redis://{config["host_redis"]}:{config["port_redis"]}')
 app = FastAPI()
 
 
-@app.get('/show/{id}')
-def show(id):
-    return {conn.get(id)}
+@app.get('/show/{id_par}')
+def show(id_par):
+    """Pylint ацтань x2"""
+    return {conn.get(id_par)}
 
 
-@app.get('/visit/{id}')
-def visit(id):
-    conn.incr(id)
+@app.get('/visit/{id_par}')
+def visit(id_par):
+    """Pylint ацтань x3"""
+    conn.incr(id_par)
     return status.HTTP_200_OK
 
 
 if __name__ == "__main__":
-    uvicorn.run(app,host = config["host"], port = config['site_port'], log_level = config['log_level'])
+    uvicorn.run(app,host = config["host"], port = config['site_port'], 
+                log_level = config['log_level'])
